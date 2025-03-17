7993586a2e2e8ecc69ba4a14dfc5aada41f2554a
rafaseara <rafaseara257@gmail.com>
2025-03-17 15:59:27 -0300

    fix: Corrigir problema na verificacao do acesso

--- modelos/usuario.py
+++ modelos/usuario.py
@@ -2,3 +2,11 @@ class Usuario:
     def __init__(self, nome, acessos):
         self._nome = nome
         self._acessos = acessos
+    
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def acessos(self):
+        return self._acessos
