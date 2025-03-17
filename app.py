24398fa8d1a7b8e0d641dd5e8edd2b10fef031c7
rafaseara <rafaseara257@gmail.com>
2025-03-17 15:52:40 -0300

    feat: Implementar função main e importações

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+from utils.json_manager import JsonManager
+from modelos.acesso import verificar_acesso_input
+
 def main():
-    pass
+    usuarios = JsonManager.carregar_permissoes('permissoes.json')
+
+    nome_usuario = input("Digite seu nome: ")
+    area_desejada = input("Digite a área que deseja acessar: ").strip()
+
+    verificar_acesso_input(nome_usuario, area_desejada, usuarios)
 
 if __name__ == "__main__":
     main()
