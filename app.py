79714744abb7dff74fc92c33c0920585ebba759d
rafaseara <rafaseara257@gmail.com>
2025-03-30 23:53:24 -0300

    feat: baixar permissoes.json

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ from fastapi import FastAPI, Query
 import json
 
 app = FastAPI()
+
+def baixar_permissoes():
+    with open("permissoes.json", "r", encoding="utf-8") as file_permissoes:
+        return json.load(file_permissoes)
