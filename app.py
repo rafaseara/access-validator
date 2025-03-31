09b78d4f90ef012c8b509cb030ea450bed966da0
rafaseara <rafaseara257@gmail.com>
2025-03-31 16:05:11 -0300

    feat: Chamar a API a partir do click do botao

--- app.py
+++ app.py
@@ -8,3 +8,19 @@ st.title("Verificando o Acesso")
 
 nome_usuario = st.text_input("Insira seu nome: ")
 lugar_usuario = st.text_input("Insira a área que quer entrar: ")
+
+def verificando_acesso():
+    if not nome_usuario or not lugar_usuario:
+        st.text("Por favor, preencha ambos os campos")
+        return
+
+    try:
+        response = requests.get(API_URL, params={"nome": nome_usuario, "lugar_acesso": lugar_usuario})
+        if response.status_code == 200:
+            st.success(response.json().get("mensagem", "Erro na resposta da API"))
+        else:
+            st.error("Erro ao conectar com a API.")
+    except Exception as erro:
+        st.error(f"Erro na conexão com a API: {erro}")
+
+st.button("Acessar", on_click=verificando_acesso)
