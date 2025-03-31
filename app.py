6d8ba734314be668fcb1a71cb79adc1af26292f6
rafaseara <rafaseara257@gmail.com>
2025-03-31 00:08:28 -0300

    feat: adicionar funcao de validar o acesso

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ app = FastAPI()
 def baixar_permissoes():
     with open("permissoes.json", "r", encoding="utf-8") as file_permissoes:
         return json.load(file_permissoes)
+
+@app.get("/api/verificar_acesso/")
+def validar_acesso_usuario(nome: str = Query(...), lugar_acesso: str = Query(...)):
+    permissoes = baixar_permissoes()
+
+    for usuario in permissoes["usuarios"]:
+        if usuario["nome"].lower() == nome.lower():
+            if lugar_acesso.lower() in map(str.lower, usuario["acessos"]):
+                mensagem = "✅ Acesso permitido!"
+                return {mensagem}
+            else:
+                mensagem = "❌ Acesso negado!"
+                return {mensagem}
+        else:
+            return "O usuário não foi encontrado, tente novamente!"
